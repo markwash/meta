4ba820b0c85bb329daca619115f3dd6025933ebb
Mark J. Washenberger <mark.washenberger@markwash.net>
2012-11-21 09:33:45 -0800

    Switch to metaclass implementation

--- meta.py
+++ meta.py
@@ -1,23 +1,56 @@
 class Property(object):
-    def __init__(self):
-        self.name = None
+    def __init__(self, name=None):
+        self.name = name
+
+    def _lookup_storage(self, obj):
+        return obj._model[self.name]
 
     def __get__(self, obj, type=None):
-        if not hasattr(obj, self.name):
-            setattr(obj, self.name, None)
-        return getattr(obj, self.name)
+        storage = self._lookup_storage(obj)
+        if not 'value' in storage:
+            return None
+        return storage['value']
 
     def __set__(self, obj, value):
-        setattr(obj, self.name, value)
+        storage = self._lookup_storage(obj)
+        storage['value'] = value
+
+
+class ModelMeta(type):
+
+    def __new__(klass, name, bases, cls_dict):
+        properties = klass._find_model_properties(cls_dict)
+        klass._plug_in_properties(properties)
+        init = klass._transform_init(properties, cls_dict.get('__init__'))
+        cls_dict['__init__'] = init
+        return super(ModelMeta, klass).__new__(klass, name, bases, cls_dict)
+
+    @classmethod
+    def _find_model_properties(klass, cls_dict):
+        return dict([(name, value) for name, value in cls_dict.items()
+                     if isinstance(value, Property)])
+
+    @classmethod
+    def _plug_in_properties(klass, properties):
+        for name, prop in properties.items():
+            if prop.name is None:
+                prop.name = name
+
+    @classmethod
+    def _transform_init(klass, properties, original_init=None):
+        def transformed_init(self, *args, **kwargs):
+            self._model = {}
+            for name, prop in properties.items():
+                self._model[name] = {}
+                if name in kwargs:
+                    setattr(self, name, kwargs.pop(name))
+            if original_init is not None:
+                original_init(self, *args, **kwargs)
+            else:
+                if len(args) > 0 or len(kwargs) > 0:
+                    raise TypeError('unexpected arguments')
+        return transformed_init
 
 
 class Base(object):
-    def __init__(self, **kwargs):
-        for name, attribute in self.__class__.__dict__.items():
-            if isinstance(attribute, Property):
-                attribute.name = '_meta_{}'.format(name)
-                if name in kwargs:
-                    setattr(self, name, kwargs.pop(name))
-        if len(kwargs) > 0:
-            message = "unexpected keyword argument '{}'"
-            raise TypeError(message.format(kwargs.popitem()[0]))
+    __metaclass__ = ModelMeta
